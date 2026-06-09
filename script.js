66e502d725ac79d3e3a7addffee4014675d3e1ae
Danielle Farias <103695058+DanielleFMedeiros@users.noreply.github.com>
2026-06-09 08:43:24 -0300

    Change condition from 'mpc' to 'opcoesmpc'

--- script.js
+++ script.js
@@ -842,7 +842,7 @@ function gerarA() {
   }
 
   // MPC
-  else if (problema2 === 'mpc') {
+  else if (problema2 === 'opcoesmpc') {
 
     const cadastro = obterValorElemento2('cadastro');
     const celular = obterValorElemento2('celular');
